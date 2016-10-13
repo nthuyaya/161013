ebcaefb0ef11deb5346b195061ea971384ecd8e4
Mac <mac@mac-106.local>
2016-10-13 11:27:11 +0800

    Initial Commit

---
+++ poem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poem
+//
+//  Created by yaya on 2016/10/13.
+//  Copyright © 2016年 yaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
